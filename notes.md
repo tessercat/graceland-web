99a96d8afb943f84c2a20e10921fe0a8ad05e4b2
Peter Christensen <tessercat@pakaa.net>
2021-11-22 15:19:32 -0700

    Add notes.

---
+++ notes.md
@@ -0,0 +1,28 @@
+Eternal because every ending is a new beginning, infinite because everything circles back to where it started, perfect because it moves indivisibly as one. Indescribably vast, it dwindles to nothing at all.
+
+Love gives us courage, saving lets us spend, not bossing people around gets the best results.
+
+```
+function longevityAndProsperity(conservation = true, cooperation = true) {
+     conservatation = adaptability;
+     adaptability = potential;
+     opportunity = false;
+     risk = false;
+     if (potential) {
+         opportunity = true;
+     }
+     if (opportunity) {
+        risk = true;
+     }
+     if (risk && cooperation) {
+         return true;
+     }
+     return false;
+}
+```
+
+We have a mission but we don't explain it. We're creating something but not to own it. We're doing something but not to save anyone. We're leading them but not to slaughter. We'll get there but it's not the promised land.
+
+Make it plain, keep it simple, be less needy, don't be greedy.
+
+Be like water. Water helps everything and takes people's shit. It's consistent, open, tolerant, kind, fair, useful and adaptable, and above all, it doesn't get angry and hold grudges.
---
+++ notes.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Notes to Self</title>
+    <style type="text/css" media="screen">
+html {
+  font: normal normal normal 1rem/1.6 sans-serif;
+  font-size: 1rem;
+}
+body {
+  color: #ced4da;
+  background: #1f2022;
+  font-size: 1rem;
+}
+main {
+  max-width: 800px;
+  margin: 0 auto 0;
+}
+main section {
+  background: black;
+  padding: 0 1rem 0;
+  border-radius: 10px;
+  border-style: solid;
+  border-width: thin;
+}
+footer {
+  margin-top: 3rem;
+  margin-bottom: 4rem;
+  font-size: smaller;
+  text-align: center;
+}
+footer a {
+  color: #ced4da;
+  text-decoration: none;
+}
+footer ul {
+  list-style: none;
+  padding: inherit;
+}
+footer li {
+  margin-bottom: 1rem;
+}
+    </style>
+  </head>
+  <body>
+    <main>
+      <section>
+<?php
+$file = '/opt/graceland/web/notes.md';
+require_once 'Michelf/MarkdownExtra.inc.php';
+use Michelf\MarkdownExtra;
+echo MarkdownExtra::defaultTransform(file_get_contents($file));
+?>
+      </section>
+   </main>
+   <footer>
+     <nav>
+       <ul>
+          <li><a href="/" title="Graceland">Home</a></li>
+          <li><a href="/contact" title="Contact Graceland">Contact</a></li>
+       </ul>
+     </nav>
+   </footer>
+  </body>
+</html>
